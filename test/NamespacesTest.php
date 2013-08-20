e3289325f06f603f19089f95b38069bf50611a9e
pvergara <pablocristobal@gmail.com>
2013-08-21 00:26:15 +0200

    Nuevo test de namespaces

--- test/NamespacesTest.php
+++ test/NamespacesTest.php
@@ -1,6 +1,7 @@
 <?php
 include_once '../classes/Ecos/ClaseConNameSpace.class.php';
 use Ecos\ClaseConNameSpace;
+use Ecos as E;
 
 
 class NamespacesTest extends PHPUnit_Framework_TestCase {	
@@ -13,6 +14,12 @@ class NamespacesTest extends PHPUnit_Framework_TestCase {
 
     public function test_AccediendoAUnaClaseDentroDeUnNsSoloPorSuNombreYGraciasAlUse(){
         $objeto = new ClaseConNameSpace();
+        
+        $this->assertEquals ( 'adios!!!', $objeto->hola() );
+    }
+
+    public function test_AccediendoAUnaClaseDentroDeUnNsUsandoElAlias(){
+        $objeto = new E\ClaseConNameSpace();
         
         $this->assertEquals ( 'adios!!!', $objeto->hola() );
     }
