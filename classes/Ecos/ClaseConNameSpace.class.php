88d93f8f7ef2199a1fa106bfde5e3a37b9fcc462
pvergara <pablocristobal@gmail.com>
2013-08-21 00:27:09 +0200

    Limpieza

--- classes/Ecos/ClaseConNameSpace.class.php
+++ classes/Ecos/ClaseConNameSpace.class.php
@@ -1,15 +1,6 @@
 <?php
 namespace Ecos;
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- * Description of ClaseConNameSpace
- *
- * @author pvergara
- */
 class ClaseConNameSpace {
     public function hola()
     {
